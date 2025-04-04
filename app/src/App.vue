a2f9d09e027e94030ec86836204d0ec041844230
Jasong237 <jasonguo4635@gmail.com>
2025-04-04 14:00:31 -0400

    Sign up stuff

--- app/src/App.vue
+++ app/src/App.vue
@@ -12,10 +12,34 @@
   onMounted(() => {
     getInstruments()
   })
+
+async function signUpNewUser() {
+  const { data, error } = await supabase.auth.signUp({
+    email: 'valid.email@supabase.io',
+    password: 'example-password',
+    options: {
+      emailRedirectTo: 'https://example.com/welcome',
+    },
+  })
+    console.log(data,error)
+}
+
+function test() {
+  console.log(username.value, password.value)
+}
+
+const username = ref("")
+const password = ref("")
   </script>
 
   <template>
     <ul>
-      <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
+      <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }} {{ instrument.id }}</li>
+      <input type="text" v-model="username" />
+      <input type="text" v-model="password" />
+      <input type="submit" @click="test()"/>
+      <p> Your Username: {{ username }} </p>
+      <p> Your password: {{ password }} </p>
     </ul>
+    
   </template>

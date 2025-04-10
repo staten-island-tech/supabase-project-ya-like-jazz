4c3050f316457e355ac7d9524477a41f00db2e29
PeterDelwita <124073562+PeterDelwita@users.noreply.github.com>
2025-04-10 13:47:13 -0400

    tailwind

--- app/src/views/RegisterWebsite.vue
+++ app/src/views/RegisterWebsite.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Hello</h1>
+    <h1 class="text-red-500">Hello</h1>
     <input type="text" v-model="username" />
     <input type="text" v-model="password" />
     <input type="submit" @click="signUpNewUser()" />

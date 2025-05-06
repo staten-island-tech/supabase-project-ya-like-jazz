4a93be4aeff866b6f9fded6ff38b6bd7fcb4f6ac
PeterDelwita <124073562+PeterDelwita@users.noreply.github.com>
2025-05-06 14:00:27 -0400

    figuring out pinia

--- app/src/stores/loggedin.ts
+++ app/src/stores/loggedin.ts
@@ -1,17 +1,13 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import type { Credentials } from "../types.ts"
 
-// interface State { // You can define the state for Pinia as an interface with Typescript 
-//     userList: Credential[]
-//     user: Credential | null
-// }
-
-// export const userStore = defineStore('user', {
-//     state: (): State => {
-//         return {
-//             userList: [],
-//             user: null,
-//         }
+// export const useUserStore = defineStore('user', {
+//     state: (): Credentials => {
+       
 //     }
 // })
+
+// State - We want to check if a user is logged in so they don't have to log in over and over every time they go to a different webpage
+// Use verified (app.vue) to verify logged-in status
 

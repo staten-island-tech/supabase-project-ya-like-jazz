d0b782f791bb8195c8433d3275cc299f4c6fe784
Jasong237 <jasonguo4635@gmail.com>
2025-05-09 14:01:43 -0400

    Theme compatibility part 1

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,18 +1,18 @@
 <template>
-<div class="min-h-screen bg-color-1" @click="listener && listenerOff()">
+<div :data-theme="currentTheme" class="min-h-screen bg-1" @click="listener && listenerOff()">
   <header>
     
-    <nav class="bg-color-2 h-16 p-2 flex justify-between items-center">
+    <nav class="bg-2 h-16 p-2 flex justify-between items-center">
       <div>
-        <RouterLink class="bg-color-3 hover:bg-color-3-hover text-color-1 py-2 px-4 rounded"
+        <RouterLink class="bg-3 hover:bg-hover3 text-1 py-2 px-4 rounded"
          to="/">Home</RouterLink>
       </div>
 
 
       <div class="flex justify-end">
-        <RouterLink v-if="!verified" class="bg-color-3 hover:bg-color-3-hover text-color-1 py-2 px-4 rounded"
+        <RouterLink v-if="!verified" class="bg-3 hover:bg-hover3 text-1 py-2 px-4 rounded"
        to="/register"> Register </RouterLink>
-       <RouterLink v-if="!verified" class="bg-color-3 hover:bg-color-3-hover text-color-1 py-2 px-4 rounded ml-2"
+       <RouterLink v-if="!verified" class="bg-3 hover:bg-hover3 text-1 py-2 px-4 rounded ml-2"
        to="/login"> Log In </RouterLink>
 
 
@@ -24,11 +24,11 @@
       A
     </div>
 
-    <div v-if="isDropdownOpen" class="absolute right-0 bg-color-2 text-white rounded-md shadow-lg mt-16 mr-4 w-40 p-2">
+    <div v-if="isDropdownOpen" class="absolute right-0 bg-2 text-white rounded-md shadow-lg mt-16 mr-4 w-40 p-2">
       <ul>
-        <li class="py-1 px-2 hover:bg-color-3-hover cursor-pointer" @click="router.push('/profile')">Profile</li>
-        <li class="py-1 px-2 hover:bg-color-3-hover cursor-pointer" @click="router.push('/settings')">Settings</li>
-        <li class="py-1 px-2 hover:bg-color-3-hover cursor-pointer" @click="signOut()">Sign Out </li>
+        <li class="py-1 px-2 hover:bg-hover cursor-pointer" @click="router.push('/profile')">Profile</li>
+        <li class="py-1 px-2 hover:bg-hover3 cursor-pointer" @click="router.push('/settings')">Settings</li>
+        <li class="py-1 px-2 hover:bg-hover3 cursor-pointer" @click="signOut()">Sign Out </li>
       </ul>
     </div>
   </div>
@@ -53,6 +53,11 @@ import { list } from 'postcss'
 const router = useRouter()
 const isDropdownOpen = ref(false)
 const listener = ref(false)
+const currentTheme = ref('default')
+
+const toggleTheme = () => {
+  currentTheme.value = currentTheme.value === 'light' ? 'dark' : 'light'
+}
 
 function toggleDropdown() {
   if (listener.value === false){
